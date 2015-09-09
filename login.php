6ff85c43eee0bb0fdfd46a31e435ac862c1df5a4
Hendrik Vallimägi <hendval@greeny.cs.tlu.ee>
2015-09-09 12:38:38 +0300

    login

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+	// LOGIN.PHP
+
+
+
+
+
+
+
+
+
+
+?>
+<html>
+<head>
+	<title>Facebook login page</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<center>
+		<h1>Logi sisse</h1>
+			<form>
+				Kasutajanimi:<br>
+				<input type="email" placeholder="E-post"><br>
+				Parool:<br>
+				<input type="password" placeholder="Parool"><br><br>
+				<input type="submit" value="Logi sisse">
+			</form>
+			<br><br>
+		<h1>Registreeri</h1>
+			<form>
+				
+			</form>
+	</center>
+</body>
+</html>
