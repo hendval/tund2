0b1383dfded15f67aa417008ce41e6d23e6f561b
Hendrik Vallimägi <hendval@greeny.cs.tlu.ee>
2015-09-09 11:33:38 +0300

    yo

--- helloworld.php
+++ helloworld.php
@@ -3,5 +3,23 @@
 	$last_name = "Vallimägi";
 	$age = 20;
 	
-	echo $first_name." ".$last_name." "."Vanus: ".$age;
+	echo $first_name." ".$last_name;
+?>
+<br>
+<?php
+	
+	// testib loogikat, juhul kui vanus on väiksem kui 18 siis kirjuta ""alaealine""
+	// muul juhul ""täisealine""
+
+	// if'i sisse loogikatehe 
+	if($age >= 18) {
+		//tõene
+		
+		echo "täisealine";
+	} else {
+		//väär
+		
+		echo "alaealine";
+	}
+	
 ?>
