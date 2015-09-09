6a37c99acef6aa8d58d1db0f144d6927bf655931
Hendrik Vallimägi <hendval@greeny.cs.tlu.ee>
2015-09-09 13:21:19 +0300

    login v2rk jo

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 
 	// LOGIN.PHP
 	$email_error = "";
+	$password_error = "";
 	// echo $_POST["email"];
 
 	// kontrollime, et keegi vajutas input nuppu
@@ -11,9 +12,22 @@
 		
 		
 		if(empty($_POST["email"]))  {
-			$email_error = "See väli on kohustuslik";
+			$email_error = "Email on kohustuslik";
 			
 			
+		}
+		
+		if(empty($_POST["password"])) {
+			$password_error = "Parool on kohustuslik";
+		} else {
+			
+			//kui oleme siia jõudnud siis parool ei ole tühi
+			//kontrollin et olek vähemalt 8 sümbolit pikk
+			if(strlen($_POST["password"]) < 8) {
+				
+				$password_error = "Parool peab olema vähemalt 8 tähemärki pikk";
+				
+			}
 		}
 		
 	}
@@ -36,7 +50,7 @@
 				Kasutajanimi:<br>
 				<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br>
 				Parool:<br>
-				<input name="password" type="password" placeholder="Parool"><br><br>
+				<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?><br><br>
 				<input type="submit" value="Logi sisse">
 			</form>
 	</center>
